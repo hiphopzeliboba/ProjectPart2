3a90181af0a050e48e0ac324c1b6e78e81707fe5
tteemmcchh <popovartimka@gmail.com>
2023-12-24 19:28:26 +0300

    dev/qa code v1.2

--- internal/methods.h
+++ internal/methods.h
@@ -1,6 +1,122 @@
 #include <string>
+#include <iostream>
+#include <chrono>
 #include <math.h>
+#include <vector>
+#include <fstream>
 
-double solve(int i, std::string line){
-    return log(std::stod(line)/i);
-}
+class OmpSolve {
+    std::string filePath = "./files/";
+    double eps = 0.2;
+public:
+    /**
+     * создает файл с определенным кол-вом строк, и заполняет строки согласно условию задачи
+     *
+     * @param fileName - название файла с значениями
+     * @param lineNum - количество строк в файле
+     * @param x - искомое значение Х
+     * **/
+    void createFiles(std::string fileName, float x, int lineNum) {
+
+        std::ofstream testFile; //создаем поток для записи
+        testFile.open(filePath + fileName); // открываем файл для записи
+        if (testFile.is_open()) { // если файл открыт для записи, то заполняем стрки по формуле
+            for (int i = 1; i <= lineNum; i++) {
+                testFile << double(i * (exp(x) + 0.001 + rand() % 1)) << "\n";
+            }
+        }
+        testFile.close();
+        std::cout << "File has been written!" << std::endl;
+
+    };
+
+    /**
+     * решение с помощью ОМП и методом МНК
+     *
+     * @param fileName - название файла с значениями
+     * @param lineNum - количество строк в файле
+     * @param threadsNum - количество потоков
+     * **/
+    double ompSolve(std::string fileName, int lineNum, int threadsNum) {
+        auto begin = std::chrono::high_resolution_clock::now();
+
+        double ySum, yxSum = 0; // переменные для суммирования значений У и У*Х, где у - значение строки, х - номер строки
+        std::vector<double> yArray{0};
+        std::string line;
+
+        std::ifstream testFile;          //создаем поток для записи
+        testFile.open(filePath + fileName); // открываем файл для записи
+        if (!testFile.is_open()) {
+            std::cerr << "Error opening text file" << std::endl;
+        }
+
+        //заполняем вектор значениями из файла
+        for (int i = 1; i <= lineNum; i++) {
+            std::getline(testFile, line);
+            yArray.push_back(stod(line));
+        }
+
+        // считаем сумму значений из файла
+#pragma omp parallel for reduction(+:ySum) num_threads(threadsNum)
+        for (int i = 0; i <= lineNum; i++) {
+            ySum += yArray[i];
+        }
+
+        //считаем сумму значений У*Х, где у-значение строки, х - сответвенное значение i (номер строки)
+#pragma omp parallel for reduction(+:yxSum) num_threads(threadsNum)
+        for (int i = 0; i <= lineNum; i++) {
+            yxSum += (yArray[i] * i);
+        }
+
+        double yAve = ySum / lineNum; // среднее значение У
+        double yxAve = yxSum / lineNum; // среднее значение у*х
+        double xAve = ((1 + lineNum) / 2 * lineNum) / lineNum; // среднее значение х
+        double xxAve = (lineNum * (lineNum + 1) * (2 * lineNum + 1) / 6) / lineNum; // среднее значение х*х
+        double B2 = (yxAve - xAve * yAve) / (xxAve - xAve * xAve); // расчет коэффициента В2
+        std::cout << "log(exp(x) = " << log(B2) << std::endl;
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<float> duration = end - begin;
+        std::cout << "Time solving - " << duration.count()<<std::endl;
+        return log(B2);
+    }
+
+    /**
+     * линейное решение без использования ОМП
+     *
+     * @param fileName - название файла с значениями
+     * @param lineNum - количество строк в файле
+     * **/
+    double simpleSolve(std::string fileName, int lineNum) {
+        auto begin = std::chrono::high_resolution_clock::now();
+        double ySum, yxSum = 0;
+        std::vector<double> yArray{0};
+        std::string line;
+        std::ifstream testFile;          //создаем поток для записи
+        testFile.open(filePath + fileName); // открываем файл для записи
+        if (!testFile.is_open()) {
+            std::cerr << "Error opening text file" << std::endl;
+        }
+
+        for (int i = 1; i <= lineNum; i++) {
+            std::getline(testFile, line);
+            yArray.push_back(stod(line));
+        }
+        for (int i = 0; i <= lineNum; i++) {
+            ySum += yArray[i];
+            yxSum += (yArray[i] * i);
+        }
+        double yAve = ySum / lineNum;
+        double yxAve = yxSum / lineNum;
+        double xAve = ((1 + lineNum) / 2 * lineNum) / lineNum;
+        double xxAve = (lineNum * (lineNum + 1) * (2 * lineNum + 1) / 6) / lineNum;
+        double B2 = (yxAve - xAve * yAve) / (xxAve - xAve * xAve);
+        std::cout << "log(exp(x) = " << log(B2) << std::endl;
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<float> duration = end - begin;
+        std::cout << "Time solving - " << duration.count()<<std::endl;
+        return log(B2);
+    }
+
+
+};
+
--- main.cpp
+++ main.cpp
@@ -6,29 +6,12 @@
 #include<chrono>
 
 
-
 int main() {
     auto begin = std::chrono::high_resolution_clock::now();
-    int j=1;
-    std::ifstream file("./files/testFile1.txt");
-    if (!file.is_open()) {
-        std::cerr << "Error opening file" << std::endl;
-        return 1;
-    }
-#pragma omp parallel num_threads(4) shared(j)
-#pragma omp for
-    for(int i =1; i<=100; ++i){
-        std::string line;
-#pragma omp critical
-        std::getline(file, line);
-#pragma omp critical
-        std::cout<<"i= "<<j<<" line= "<<line<<" res= "<< solve(j,line)<<" thread= "<<omp_get_thread_num()<<std::endl;
-        ++j;
-    }
+    OmpSolve solve;
+    solve.createFiles("testFile2.txt", 3, 400);
+    double testX = solve.ompSolve("testFile2.txt", 400, 4);
+    double test2X = solve.simpleSolve("testFile2.txt", 400);
 
-    file.close();
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<float> duration = end -begin;
-    std::cout<<duration.count();
     return 0;
 }
